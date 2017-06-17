45590328dbafe471e869b078f7b92ce18a50d1f8
ashasamp47 <asha.naganur@gmail.com>
2017-06-17 16:55:03 +1000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Even
+    {
+        static void Main(string[] args)
+        {
+
+            int j = 200;
+            for (int i = 0; i < j; i = i+2 )
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
