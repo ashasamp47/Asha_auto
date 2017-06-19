3eb8f84ff2f6a374ba770d17fade05cb0ede9312
ashasamp47 <asha.naganur@gmail.com>
2017-06-19 16:16:50 +1000

    Add files via upload
    through function

---
+++ Print_Even.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            PrintEven.Even();
+
+            Console.ReadLine();
+        }
+
+    }
+
+    class PrintEven
+    {
+        static int i; int j;
+        public static void Even()
+        {
+            int j = 200;
+
+            for (int i = 0; i < j; i = i + 2)
+            {
+                Console.WriteLine(i);
+            }
+
+
+        }
+    }
+
+}
